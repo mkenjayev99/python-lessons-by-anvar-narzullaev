5debc91c1b6eac055680169addd2b6adab2c64ca
m_kenjayev <musulmonxon@mail.ru>
2022-06-01 12:55:27 +0500

    simple transliteratorBot created and fixed

--- Python darslari/pyTelegramBot/simpleBot_transliterator.py
+++ Python darslari/pyTelegramBot/simpleBot_transliterator.py
@@ -1,19 +1,24 @@
 from transliterate import to_cyrillic, to_latin
-from pyTelegramBotAPI import telebot
+import telebot
 
-token = '5427270655:AAGE2l0T5WV4gPdeib5allkKS7AAgCsMC-U'
-bot = telebot.TeleBot(token, parse_mode = None)
+TOKEN = '5427270655:AAGE2l0T5WV4gPdeib5allkKS7AAgCsMC-U'
+bot = telebot.TeleBot(token = TOKEN)
+
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    answer = "Assalomu alaykum\nXush keldingiz!"
+    username = (
+        message.from_user.username
+    )
+    answer = f"Assalomu alaykum {username}! \nXush keldingiz!"
     bot.reply_to(message, answer)
 
-@bot.message_handler(func = lambda message: True)
-def echo_all(message):
+
+@bot.message_handler(func=lambda msg: msg.text is not None)
+def translit(message):
     msg = message.text
     answer = lambda msg: to_cyrillic(msg) if msg.isascii() else to_latin(msg)
 
-    bot.reply_to(message, answer)
+    bot.reply_to(message, answer(msg))
 
 bot.polling()
